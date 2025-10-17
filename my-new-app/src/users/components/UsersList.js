418a85862a202bac0138f5eb1f42e5250b16f6ee
joaopedro0411 <joaopedrocarneirodafonseca@gmail.com>
2025-10-17 17:43:30 -0300

    Aula 47

--- my-new-app/src/users/components/UsersList.js
+++ my-new-app/src/users/components/UsersList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './UsersList.css'
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 const UsersList = (props) => {
     if (props.items.length === 0) {
         return (
--- my-new-app/src/users/pages/Users.js
+++ my-new-app/src/users/pages/Users.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import UsersList from "../components/UsersList";
 const Users = () => {
-    const USERS =  [{id: 'u2', name:'Jack', image: 'nothing', places:3}];
-    return <UsersList items={USERS}/>;
+    const USERS = [{ 
+          id: 'u2',
+          name: 'Jack',
+          image: 'https://www.creativefabrica.com/wp-content/uploads/2023/05/23/Bearded-man-avatar-Generic-male-profile-Graphics-70342414-1.png', 
+          places: 3 }];
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
